11c603465124edef4dae9def95d334f8f5047243
israelaguila3039-star <israel.aguila3039@stu.mtec.edu>
2026-03-23 07:31:10 -0600

    goodbye extra

--- positiveTest/controlArea/whereItHappens.js
+++ positiveTest/controlArea/whereItHappens.js
@@ -1,10 +1,13 @@
 import { expect } from '@wdio/globals'
 import { $ } from '@wdio/globals'
 import thePlaceILogIn from '../stuff/loggingIn.js'
+//inporting the class 
 
 
 async function theTestIng(username, password, passSelector) {    
-    
+//This function can control what is inputted for the password, username, and the selector to determine if the test has passed.
+//This is due to neeeding to run multiple tests where
+
     describe('My Login application', () => {
         it('should login with valid credentials', async () => {
             await thePlaceILogIn.open()
@@ -17,10 +20,12 @@ async function theTestIng(username, password, passSelector) {
     })
 
 }
+//positive tests
 theTestIng("standard_user", "secret_sauce", '[class="app_logo"]');
-
-
+theTestIng();
 theTestIng("problem_user", "secret_sauce", '[class="app_logo"]');
-
+theTestIng();
 theTestIng("error_user", "secret_sauce", '[class="app_logo"]');
 theTestIng("visual_user", "secret_sauce", '[class="app_logo"]');
+
+//Negative tests
