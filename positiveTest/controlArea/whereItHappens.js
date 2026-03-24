8af3bd7099f5ab060c808414bfec3fcb8ec9df1a
israelaguila3039-star <israel.aguila3039@stu.mtec.edu>
2026-03-24 07:25:45 -0600

    maybe the end

--- positiveTest/controlArea/whereItHappens.js
+++ positiveTest/controlArea/whereItHappens.js
@@ -11,7 +11,8 @@ import thePlaceILogIn from '../stuff/loggingIn.js'
 //All the valid usernames
 const usernames = ["standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user"];
 
-async function theTestIng(username, password, passSelector, stringNeed, passText) {    
+
+async function theTestIngLogin(username, password, passSelector, stringNeed, passText) {    
     
     describe('My Login application', () => {
         it('should login with valid credentials', async () => {
@@ -29,52 +30,54 @@ async function theTestIng(username, password, passSelector, stringNeed, passText
 }
 
 
-//Positive Tests:
+
+//Login Positive Tests:
 
 
-for (let i = 0; i < 6; i++){
+for (let i = 0; i < usernames.length; i++){
     if (i == 1){
-        theTestIng(usernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Sorry, this user has been locked out.');
+        theTestIngLogin(usernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Sorry, this user has been locked out.');
     } else if (i == 2 || i == 5) {
-        theTestIng(usernames[i], "secret_sauce", '[src="/static/media/sl-404.168b1cce10384b857a6f.jpg"]', false, null);
+        theTestIngLogin(usernames[i], "secret_sauce", '[src="/static/media/sl-404.168b1cce10384b857a6f.jpg"]', false, null);
     } else {
-        theTestIng(usernames[i], "secret_sauce", '[class="app_logo"]', true, 'Swag Labs');
+        theTestIngLogin(usernames[i], "secret_sauce", '[class="app_logo"]', true, 'Swag Labs');
     }
 }
 
 
 
-//Negative Tests:
+//Login Negative Tests:
 
 
 //Incorrect password
-for (let i = 0; i < 6; i++){
-    theTestIng(usernames[i], "ppppppp", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
+for (let i = 0; i < usernames.length; i++){
+    theTestIngLogin(usernames[i], "ppppppp", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
 }
 
 //Removing "_" from username and created a value for it
 const _lessUsernames = usernames.map(str => { return str.replaceAll('_', '') });
-for (let i = 0; i < 6; i++){
-    theTestIng(_lessUsernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
+for (let i = 0; i < usernames.length; i++){
+    theTestIngLogin(_lessUsernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
 }
 
 //Wrong capitalization
 const capiUsernames = usernames.map(element => { return element.toUpperCase() });
 //Note to self: Remember to put the [i] so that you do not take 20 minutes trying to solve an issue that does not actually exist
-for (let i = 0; i < 6; i++){
-    theTestIng(capiUsernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
+for (let i = 0; i < usernames.length; i++){
+    theTestIngLogin(capiUsernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
 }
 
 //No password
-for (let i = 0; i < 6; i++){
-    theTestIng(usernames[i], "", '[class="error-message-container error"]', true, 'Epic sadface: Password is required');
+for (let i = 0; i < usernames.length; i++){
+    theTestIngLogin(usernames[i], "", '[class="error-message-container error"]', true, 'Epic sadface: Password is required');
 }
 
 //No username
-theTestIng("", "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username is required')
+theTestIngLogin("", "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username is required')
 
 //Replace all "_" with " "
 const spaceUsernames = usernames.map(str => { return str.replaceAll('_', ' ') });
-for (let i = 0; i < 6; i++){
-    theTestIng(spaceUsernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
+for (let i = 0; i < usernames.length; i++){
+    theTestIngLogin(spaceUsernames[i], "secret_sauce", '[class="error-message-container error"]', true, 'Epic sadface: Username and password do not match any user in this service');
 }
+
--- positiveTest/stuff/theEndpointAtTheTop.js
+++ positiveTest/stuff/theEndpointAtTheTop.js
@@ -2,7 +2,7 @@ import { browser } from '@wdio/globals'
 
 //This is just the endpoint we're using
 export default class uRL {
-    open () {
-        return browser.url(`https://www.saucedemo.com/`);
+    open (path = '') {
+        return browser.url(`https://www.saucedemo.com/${path}`);
     }
 }
