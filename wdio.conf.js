a62f9fb0bcc111f3f2c2f01566609700244f1a11
israelaguila3039-star <israel.aguila3039@stu.mtec.edu>
2026-03-26 09:58:25 -0600

    the first

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ export const config = {
     // of the config file unless it's absolute.
     //
     specs: [
-        './positiveTest/controlArea/**/*.js'
+        './coverageTest/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
